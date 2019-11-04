9cc231bb4d18b56b0306cc7ca282a3a8ccd5f8dd
lr <wogie636@gmail.com>
2019-11-04 21:15:36 +0100

    Option to paste fasta directly into terminal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import time
 from pathlib import Path
 
@@ -9,14 +10,20 @@ from BacteriocinClassifier import BacteriocinClassifier
 from ELMo import encode_input_fasta
 
 
-def main(cuda_device, fasta_input_file, csv_output_file):
+def main(cuda_device, fasta_input_file, csv_output_file, fasta_input_paste=False):
     if cuda_device is None:
         cuda_device = 0 if torch.cuda.is_available() else -1
         print(f'No cuda device specified. Using cuda device {cuda_device}')
 
-    if fasta_input_file is None:
+    if fasta_input_file is None and not fasta_input_paste:
         fasta_input_file = "./test_data/sample_fasta.faa"
         print(f"No input fasta file specified. Using {fasta_input_file}")
+
+    elif fasta_input_paste:
+        fasta_input_file = "temp_fasta.faa"
+        fasta_paste = input("Paste fasta file:\n")
+        with open(fasta_input_file, 'w') as f:
+            f.write(fasta_paste)
 
     if csv_output_file is None:
         csv_output_file = "./results.csv"
@@ -24,6 +31,8 @@ def main(cuda_device, fasta_input_file, csv_output_file):
 
     inpt = encode_input_fasta(input_fasta=Path(fasta_input_file),
                               cuda_device=cuda_device)
+
+    os.remove("temp_fasta.faa") if fasta_input_paste else None
 
     net: BacteriocinClassifier = BacteriocinClassifier()
     net.load_state_dict(torch.load("./weights/bacteriocin_classifier_params.dump"))
@@ -47,9 +56,12 @@ if __name__ == "__main__":
                         help='Input fasta file with sequences to classify', type=str)
     parser.add_argument("-o", "--csv_output_file", help='Where to output csv file with results',
                         type=str)
+    parser.add_argument("-p", "--fasta_input_paste",
+                        help='Flag to get input fasta from pasting into terminal',
+                        action="store_true")
     parser.add_argument("--mode", help='not used')
     parser.add_argument("--port", help='not used')
     args = parser.parse_args()
 
     main(cuda_device=args.cuda_device, fasta_input_file=args.fasta_input_file,
-         csv_output_file=args.csv_output_file)
+         csv_output_file=args.csv_output_file, fasta_input_paste=args.fasta_input_paste)
