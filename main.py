e607cfdf581c67ebe30a170923bd51111fc360a2
lr <wogie636@gmail.com>
2019-11-04 16:04:52 +0100

    Built command line interface

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import torch
@@ -7,11 +8,20 @@ from BacteriocinClassifier import BacteriocinClassifier
 from ELMo import encode_input_fasta
 
 
-def main(cuda_device=None):
+def main(cuda_device, fasta_input_file, csv_output_file):
     if cuda_device is None:
         cuda_device = 0 if torch.cuda.is_available() else -1
+        print(f'No cuda device specified. Using cuda device {cuda_device}')
 
-    inpt = encode_input_fasta(input_fasta=Path("./test_data/sample_fasta.faa"),
+    if fasta_input_file is None:
+        fasta_input_file = "./test_data/sample_fasta.faa"
+        print(f"No input fasta file specified. Using {fasta_input_file}")
+
+    if csv_output_file is None:
+        csv_output_file = "./results.csv"
+        print(f"No output path specified. Using {csv_output_file}")
+
+    inpt = encode_input_fasta(input_fasta=Path(fasta_input_file),
                               cuda_device=cuda_device)
 
     net: BacteriocinClassifier = BacteriocinClassifier()
@@ -20,8 +30,21 @@ def main(cuda_device=None):
 
     outpt = net(inpt)
 
-    return DataFrame(outpt.detach().numpy())
+    # Save results
+    (DataFrame(outpt.detach().numpy(), columns=['not_bacteriocin_score', 'bacteriocin_score'])
+     .to_csv(csv_output_file))
 
 
 if __name__ == "__main__":
-    csv_output = main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cuda_device", help='Specify which cuda device to use', type=int)
+    parser.add_argument("-f", "--fasta_input_file",
+                        help='Input fasta file with sequences to classify', type=str)
+    parser.add_argument("-o", "--csv_output_file", help='Where to output csv file with results',
+                        type=str)
+    parser.add_argument("--mode", help='not used')
+    parser.add_argument("--port", help='not used')
+    args = parser.parse_args()
+
+    main(cuda_device=args.cuda_device, fasta_input_file=args.fasta_input_file,
+         csv_output_file=args.csv_output_file)
